1894bae9a416a2e03c0b0fae2ffa4172cb1d8229
safaaaz <58644583+safaaaz@users.noreply.github.com>
2022-10-30 16:11:13 +0200

    add client class

--- service/AuthService.java
+++ service/AuthService.java
@@ -1,21 +1,23 @@
 package service;
 
+import java.util.InputMismatchException;
 import java.util.Map;
 
 public class AuthService {
-    private Map<Integer,String> tokens;
+    private Map<String,User> tokens;
 
     public String validateUserLogin(int userId,String userPassword){
-        Map<Integer,User> usersData = getUsersData();
-        if(usersData.get(userId).getPassword()!=userPassword)
-            return null;
-        return "token"; //to save token to user in tokens and return it
+        Map<Integer,User> usersData = UserRepo.getUsersData();
+        if(usersData.get(userId)!=null)
+            if(usersData.get(userId).getPassword()==userPassword)
+                return "token";//save token to user in tokens and return it
+        return null;
     }
-    public String valideteUserRegister(User user){
-        Map<Integer,User> usersData = getUsersData();
-        if(usersData.get(user.id)!=null)
-            if(usersData.get(user.id).getEmail()!=user.getEmail()){
-                saveNewUser(user);
+    public String validateUserRegister(User user){
+        Map<Integer,User> usersData = UserRepo.getUsersData();
+        if(usersData.get(user.getId())!=null)
+            if(usersData.get(user.getId()).getEmail()!=user.getEmail()){
+                UserRepo.saveNewUser(user);
                 return validateUserLogin(user.getId(),user.getPassword());
             }
         throw new RuntimeException("Id or Email has been used!");
---
+++ service/Client.java
@@ -0,0 +1,7 @@
+package service;
+
+public class Client {
+    public static void main(String[] args) {
+
+    }
+}
