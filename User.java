acb9d751360c9a687b5e964caf8e3ab8296adf21
safaaaz <58644583+safaaaz@users.noreply.github.com>
2022-10-30 13:53:05 +0200

    add user class

--- User.java
+++ User.java
@@ -1,2 +1,13 @@
 public class User {
+    private final int id;
+    private final String email;
+    private final String name;
+    private String password;
+
+    public User(int id, String email, String name, String password) {
+        this.id = id;
+        this.email = email;
+        this.name = name;
+        this.password = password;
+    }
 }
