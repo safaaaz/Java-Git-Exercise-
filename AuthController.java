3a655a5e315854a7b43d0fc342fc8bccd14be287
ShaiLevi23 <shailevi23@gmail.com>
2022-10-30 21:29:51 +0200

    fixed some exceptions

--- AuthController.java
+++ AuthController.java
@@ -3,44 +3,34 @@ import java.util.regex.Pattern;
 
 public class AuthController {
 
+    private final String regexEmail = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\\\.[A-Za-z0-9_-]+)*@[^-][A-Za-z0-9-]+(\\\\.[A-Za-z0-9-]+)*(\\\\.[A-Za-z]{2,})$";
+
     public boolean validateUserInput(User user){
-        boolean id = isValidUserId(user.getId());
-        boolean email = isValidUserEmail(user.getEmail());
-        boolean name = isValidUserName(user.getName());
-        boolean password = isValidPassword(user.getPassword());
-        if(id && email && name && password)
-            return true;
-        else
-            return false;
-        }
+        return isValidUserId(user.getId()) && isValidUserEmail(user.getEmail())
+                && isValidUserName(user.getName()) && isValidPassword(user.getPassword());
+    }
 
 
     private boolean isValidUserId(int id){
-        if (id >0) {
-            return true;
-        }
-        else {
+        if (id < 0) {
             throw new IllegalArgumentException("Id invalid");
         }
-
+        return true;
     }
     private boolean isValidUserEmail(String email){
-        String regexPattern = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\\\.[A-Za-z0-9_-]+)*@[^-][A-Za-z0-9-]+(\\\\.[A-Za-z0-9-]+)*(\\\\.[A-Za-z]{2,})$";
-        if (Pattern.compile(regexPattern).matcher(email).matches()){
-            return true;
-        }else {
+        if (!Pattern.compile(regexEmail).matcher(email).matches()) {
             throw new IllegalArgumentException("Email invalid");
         }
+        return true;
     }
 
     private boolean isValidUserName (String name){
         String regex = "^[A-Za-z]\\w{5,29}$";
         Pattern p = Pattern.compile(regex);
-        if (p.matcher(name).matches()){
-            return true;
-        }else {
+        if (!p.matcher(name).matches()){
             throw new IllegalArgumentException("Name invalid");
         }
+        return true;
     }
 
     // Function to validate the password.
@@ -52,10 +42,9 @@ public class AuthController {
                 + "(?=\\S+$).{8,20}$";
 
         Pattern p = Pattern.compile(regex);
-        if (p.matcher(password).matches()){
-            return true;
-        }else {
+        if (!p.matcher(password).matches()){
             throw new IllegalArgumentException("Password invalid");
         }
+        return true;
     }
 }
--- UserRepo.java
+++ UserRepo.java
@@ -22,29 +22,29 @@ public class UserRepo {
         try {
             this.file = new File("./users.json");
         } catch (NullPointerException e) {
-            throw new RuntimeException("Something went wrong, no database file found", e);
+            throw new RuntimeException("Something went wrong, database file not found", e);
         }
     }
 
-    void writeToFile(Map<Integer,User> usersData){
+    void writeToFile(Map<Integer,User> usersData) throws FileNotFoundException {
         try(FileOutputStream fileOut = new FileOutputStream(this.file.getName())) {
             fileOut.write(gson.toJson(usersData).getBytes());
             fileOut.flush();
         }
         catch (IOException e) {
-            throw new RuntimeException(e);
+            throw new FileNotFoundException("Can't write to file");
         }
     }
 
 
 
-    Map<Integer, User> getUsersData(){
+    Map<Integer, User> getUsersData() throws FileNotFoundException {
         try{
             String result = new String(Files.readAllBytes(Paths.get("./users.json")));
             return this.gson.fromJson(result, new TypeToken<HashMap<Integer, User>>() {}.getType());
         }
         catch (IOException e) {
-            throw new RuntimeException(e);
+            throw new FileNotFoundException("Can't read from file");
         }
     }
 }
