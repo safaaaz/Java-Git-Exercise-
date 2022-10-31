4e3d6ab0b8afe1b45e587da58ef20dc86ec92bcc
Yael Adler <yaeli.adler@gmail.com>
2022-10-31 15:21:13 +0200

    Beggining of UserService

--- service/UserService.java
+++ service/UserService.java
@@ -1,9 +1,8 @@
+package service;
+
 import java.util.Map;
 
 public class UserService {
     Map<Integer, User> map;
 
-    public UserService(Map<Integer, User> map) {
-        this.map = map;
-    }
 }
