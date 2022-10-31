5d8f3ac9fa9466c11b6ab5e375a035530fdedbce
safaaaz <58644583+safaaaz@users.noreply.github.com>
2022-10-31 16:17:50 +0200

    add deleteUser and updateUserEmail methods to userController

--- service/UserController.java
+++ service/UserController.java
@@ -1,8 +1,25 @@
 package service;
 
 public class UserController {
+    UserController userController;
 
-    public void deleteUser(String token,String userEmail){
+    public UserController() {
+        this.userController = new UserController();
+    }
 
+    public void deleteUser(String token, String userEmail){
+        if(AuthService.getInstance().getTokens().get(token)==userEmail){
+            userController.deleteUser(userEmail);
+            System.out.println("the user with email : "+userEmail+" deleted successfully ");
+            return;
+        }
+        throw new IllegalArgumentException("Incorrect token or email!");
+    }
+    public void updateUserEmail(String token,String userEmail, String newEmail ){
+        if(AuthService.getInstance().getTokens().get(token)==userEmail){
+            userController.updateUserEmail(userEmail,newEmail);
+            System.out.println("User email updated from "+userEmail +" to "+newEmail);
+        }
+        throw new IllegalArgumentException("Incorrect token or email!");
     }
 }
