f1af9a062788bce042d558c91e8d793e2d4811a3
Yael Adler <yaeli.adler@gmail.com>
2022-10-31 16:15:37 +0200

    created setters to class user

--- service/User.java
+++ service/User.java
@@ -28,4 +28,16 @@ public class User {
     public String getPassword() {
         return password;
     }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
