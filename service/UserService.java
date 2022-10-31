3aef74a02ce44d42a2529b9ae029db9920b3829c
Yael Adler <yaeli.adler@gmail.com>
2022-10-31 15:57:10 +0200

    Update to delete user function and isUser who checks that the user exists

--- service/UserService.java
+++ service/UserService.java
@@ -9,13 +9,19 @@ public class UserService {
         this.map = map;
     }
 
-    protected User deleteUser(String userEmail){
+    private User isUser(String userEmail){
         User userExists = map.get(userEmail);
         if (userEmail == null)
             throw new IllegalArgumentException("User doesn't exists");
         else
             return userExists;
     }
+    protected User deleteUser(String userEmail){
+        User userExists = isUser(userEmail);
+        return map.remove(userEmail);
+    }
 
+    protected User updateUserEmail(User user , String email){
 
+    }
 }
