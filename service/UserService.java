868cc3d64305b9dab617e7dbfe6dad63752e68b9
Yael Adler <yaeli.adler@gmail.com>
2022-10-31 15:45:21 +0200

    deleteUser function first try

--- service/UserService.java
+++ service/UserService.java
@@ -3,6 +3,19 @@ package service;
 import java.util.Map;
 
 public class UserService {
-    Map<Integer, User> map;
+    Map<String, User> map;
+
+    public UserService(Map<String, User> map) {
+        this.map = map;
+    }
+
+    protected User deleteUser(String userEmail){
+        User userExists = map.get(userEmail);
+        if (userEmail == null)
+            throw new IllegalArgumentException("User doesn't exists");
+        else
+            return userExists;
+    }
+
 
 }
