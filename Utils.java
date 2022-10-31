d414f0aa7c296233d6349e374813c3ddf71a7be9
ShaiLevi23 <shailevi23@gmail.com>
2022-10-31 10:54:28 +0200

    Added Utils class

---
+++ Utils.java
@@ -0,0 +1,5 @@
+public class Utils {
+    public String regexEmail = "^(.+)@(.+)$";
+    public String regexUsername = "^[a-zA-Z0-9]([._-](?![._-])|[a-zA-Z0-9]){3,18}[a-zA-Z0-9]$";
+    public String regexPassword = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$";
+}
